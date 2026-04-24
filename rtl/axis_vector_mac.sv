3df948fac94e78b8b11622e81f807619ce8474b9
abarajithan11 <abarajithan07@gmail.com>
2026-04-24 12:09:30 -0700

    Simplify stuff

--- rtl/axis_vector_mac.sv
+++ rtl/axis_vector_mac.sv
@@ -1,63 +1,36 @@
 `timescale 1ns/1ps
 
 module axis_vector_mac #(
-  parameter R  = 4,
-  parameter WK = 8,
-  parameter WX = 8,
-  parameter WA = 32,
-  parameter WY = 32
+  parameter R  = 4, WK = 8, WX = 8, WA = 32, WY = 32
 )(
-  input  wire                  clk,
-  input  wire                  rstn,
-
-  input  wire                  s_valid,
-  output wire                  s_ready,
-  input  wire                  s_last,
-
-  input  wire [R*WK-1:0]       sk_data,
-  input  wire [R*WX-1:0]       sx_data,
-  input  wire [R*WA-1:0]       sa_data,
-
-  output wire                  m_valid,
-  input  wire                  m_ready,
-  output wire                  m_last,
-  output wire [R*WY-1:0]       m_data
+  input  logic clk, rstn, 
+  input  logic s_valid, s_last, m_ready,
+  input  logic [R*WK-1:0]       sk_data,
+  input  logic [R*WX-1:0]       sx_data,
+  input  logic [R*WA-1:0]       sa_data,
+  output logic m_valid, m_last, s_ready,
+  output logic [R*WY-1:0]       m_data
 );
+  logic stall, en;
 
-  reg m_valid_reg;
-  reg m_last_reg;
-
-  wire stall = m_valid_reg & !m_ready;
-  wire en    = !stall;
-
-  assign s_ready = en;
-  assign m_valid = m_valid_reg;
-  assign m_last  = m_last_reg;
-
+  always_comb begin
+    stall = m_valid_reg & !m_ready;
+    en    = !stall;
+    s_ready = en;
+  end
+  
   always @(posedge clk) begin
     if (!rstn) begin
-      m_valid_reg <= 1'b0;
-      m_last_reg  <= 1'b0;
+      m_valid <= 1'b0;
+      m_last  <= 1'b0;
     end else if (en) begin
-      m_valid_reg <= s_valid;
-      m_last_reg  <= s_valid & s_last;
+      m_valid <= s_valid;
+      m_last  <= s_valid & s_last;
     end
   end
 
   vector_mac #(
-    .R (R ),
-    .WK(WK),
-    .WX(WX),
-    .WA(WA),
-    .WY(WY)
-  ) VECTOR_MAC (
-    .clk    (clk),
-    .rstn   (rstn),
-    .en     (en),
-    .sk_data(sk_data),
-    .sx_data(sx_data),
-    .sa_data(sa_data),
-    .m_data (m_data)
-  );
+    .R(R),.WK(WK),.WX(WX),.WA(WA),.WY(WY)
+    ) VECTOR_MAC (.*);
 
 endmodule
